de9e03a39544cc91242fa04fe2ab058aca880e26
sqwarlock <sqwarlock@gmail.com>
2016-06-08 16:57:40 -0700

    Update sqwarScript.js

--- scripts/sqwarScript.js
+++ scripts/sqwarScript.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
 	if (typeof window.orientation !== "undefined") {
 		$('#forMobile').addClass("mobileContainer").removeClass("mainContainer");
 		$('.centerFooter').addClass("mobileFooter").removeClass("centerFooter");
+	} else {
+		$(window).resize(function() {
+			if ($(window).width() < 960) {
+				$('.mainContainer').css('width', '99%');
+				$('.centerFooter').css('width', '99%');
+			} else {
+				$('.mainContainer').css('width', '60%');
+				$('.centerFooter').css('width', '60%');
+			}
+		});
 	}
 });
