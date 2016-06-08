d5d7e6fcffe2eca09c2cb046b77ad80142ebe651
sqwarlock <sqwarlock@gmail.com>
2016-06-08 16:44:49 -0700

    Update sqwarScript.js

--- scripts/sqwarScript.js
+++ scripts/sqwarScript.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	if (typeof window.orientation !== "undefined") {
 		$('#forMobile').addClass("mobileContainer").removeClass("mainContainer");
-		$('.contentFooter').addClass("mobileFooter").removeClass("contentFooter");
+		$('.centerFooter').addClass("mobileFooter").removeClass("centerFooter");
 	}
 });
