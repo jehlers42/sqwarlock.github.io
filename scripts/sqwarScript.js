1713970d49b2ed7fa3751456597433c05f7dec13
sqwarlock <sqwarlock@gmail.com>
2016-06-08 16:32:50 -0700

    Update sqwarScript.js

--- scripts/sqwarScript.js
+++ scripts/sqwarScript.js
@@ -7,10 +7,13 @@ $(document).ready(function() {
 			$(".mainContainer").css('width', '60%');
 			$(".centerFooter").css('width', '60%');
 		}
+	var cachedWidth = $(window).width();
 	$(window).resize(function() {
-		if ($(window).width() < 960) {
+		var newWidth = $(window).width();
+		if (newWidth != cachedWidth)) {
 			$(".mainContainer").css('width', '99%');
 			$(".centerFooter").css('width', '99%');
+			cachedWidth = newWidth;
 		}
 		else {
 			$(".mainContainer").css('width', '60%');
