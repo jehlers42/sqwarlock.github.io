f808e41a4c15976d9751436990a64087d010f015
sqwarlock <sqwarlock@gmail.com>
2016-06-08 14:59:14 -0700

    Update sqwarScript.js

--- scripts/sqwarScript.js
+++ scripts/sqwarScript.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+	$(window).resize(function() {
+		if ($(window).width() < 960) {
+			$(".mainContainer").css('width', '100%');
+		}
+		else {
+			$(".mainContainer").css('width', '60%');
+		}
+	});
 });
