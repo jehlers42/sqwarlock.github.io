a70d25ab7b845be1f209d3a8335f6e48e4130887
sqwarlock <sqwarlock@gmail.com>
2016-06-10 18:08:51 -0700

    Add files via upload

--- scripts/sqwarScript.js
+++ scripts/sqwarScript.js
@@ -1,16 +1,52 @@
 $(document).ready(function() {
+	$('.centerContentArt').hide();
+	$('.centerContentDesign').hide();
+	//Code for mobile rendering. Includes fancy resize script//
 	if (typeof window.orientation !== "undefined") {
 		$('#forMobile').addClass("mobileContainer").removeClass("mainContainer");
-		$('.centerFooter').addClass("mobileFooter").removeClass("centerFooter");
+		$('.siteFooter').addClass("mobileFooter").removeClass("siteFooter");
 	} else {
 		$(window).resize(function() {
 			if ($(window).width() < 960) {
 				$('.mainContainer').css('width', '99%');
-				$('.centerFooter').css('width', '99%');
+				$('.siteFooter').css('width', '99%');
 			} else {
 				$('.mainContainer').css('width', '60%');
-				$('.centerFooter').css('width', '60%');
+				$('.siteFooter').css('width', '60%');
 			}
 		});
 	}
+	//Load Home Page//
+	$('#home').click(function() {
+		$('.centerContentArt').fadeOut('400');
+		$('.centerContentDesign').fadeOut('400');
+		$('.centerContentHome').delay('400').fadeIn('400');
+	});
+	$('#home').hover(function() {
+		$(this).css('color', '#cc0066');
+	}, function() {
+		$(this).css('color', '#080808');
+	});
+	//Load Art Page//
+	$('#art').click(function() {
+		$('.centerContentHome').fadeOut('400');
+		$('.centerContentDesign').fadeOut('400');
+		$('.centerContentArt').delay('400').fadeIn('fast');
+	});
+	$('#art').hover(function() {
+		$(this).css('color', '#cc0066');
+	}, function() {
+		$(this).css('color', '#080808');
+	});
+	//Load Game Design Page//
+	$('#design').click(function() {
+		$('.centerContentHome').fadeOut('400');
+		$('.centerContentArt').fadeOut('400');
+		$('.centerContentDesign').delay('400').fadeIn('fast');
+	});
+	$('#design').hover(function() {
+		$(this).css('color', '#cc0066');
+	}, function() {
+		$(this).css('color', '#080808');
+	});
 });
